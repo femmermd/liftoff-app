0f0cfc240e221828541b829b5bdb9106a869d7a0
Matt Femmer <femmermd@gmail.com>
2019-08-13 20:08:25 -0500

    basic build

---
+++ src/main/java/org/launchcode/liftoffapp/LiftoffAppApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.liftoffapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftoffAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiftoffAppApplication.class, args);
+	}
+
+}
