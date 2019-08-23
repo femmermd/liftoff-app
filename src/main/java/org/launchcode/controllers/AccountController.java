22c6fae6c2c76a43db79d03f9c4e70033e331a85
Matt Femmer <femmermd@gmail.com>
2019-08-23 08:38:40 -0500

    Created account controller Rendered login and register pages at /login and /register

---
+++ src/main/java/org/launchcode/controllers/AccountController.java
@@ -0,0 +1,22 @@
+package org.launchcode.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(value="")
+public class AccountController {
+
+    @RequestMapping(value = "/login")
+    public String login(){
+        return "login";
+    }
+
+    @RequestMapping(value = "/register")
+    public String register(){
+        return "register";
+    }
+}
