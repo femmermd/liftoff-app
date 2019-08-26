b5ee7c6349d0ed63e0a5cf3cf527b62bd18d3ca6
Matt Femmer <femmermd@gmail.com>
2019-08-26 07:41:49 -0500

    Added fields, getters and setters to User model

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,9 +1,8 @@
 package org.launchcode.models;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -18,6 +17,23 @@ public class User {
     @GeneratedValue
     private int id;
 
+    @NotNull
+    @Size(min=6, max = 20)
+    private String password;
+
+  /*  @ManyToMany
+    private List<Review> reviews;*/
+
+    @NotNull
+    @Email
+    private String email;
+
+    private String hometown;
+
+    @OneToOne
+    private int photoId;
+
+
     public User(){};
 
     public User(String userName){
@@ -25,8 +41,59 @@ public class User {
         this.userName=userName;
     }
 
-    
+    public String getUserName() {
+        return userName;
+    }
 
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+/*    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }*/
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getHometown() {
+        return hometown;
+    }
+
+    public void setHometown(String hometown) {
+        this.hometown = hometown;
+    }
+
+    public int getPhotoId() {
+        return photoId;
+    }
+
+    public void setPhotoId(int photoId) {
+        this.photoId = photoId;
+    }
 }
