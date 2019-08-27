deae9402e3909956ae2f168a0a676cd1660651f5
Matt Femmer <femmermd@gmail.com>
2019-08-26 20:58:42 -0500

    Added UserDao

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -11,7 +11,7 @@ public class User {
 
     @NotNull
     @Size(min=3,max=25)
-    private String userName;
+    private String username;
 
     @Id
     @GeneratedValue
@@ -36,17 +36,17 @@ public class User {
 
     public User(){};
 
-    public User(String userName){
+    public User(String username){
         this();
-        this.userName=userName;
+        this.username = username;
     }
 
-    public String getUserName() {
-        return userName;
+    public String getUsername() {
+        return username;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public int getId() {
