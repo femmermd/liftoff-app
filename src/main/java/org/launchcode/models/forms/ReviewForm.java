a6f1b1cbfefd3f739b963430f7b8d66b1f5a8e27
Matt Femmer <femmermd@gmail.com>
2019-09-11 08:08:46 -0500

    created ReviewForm object

---
+++ src/main/java/org/launchcode/models/forms/ReviewForm.java
@@ -0,0 +1,118 @@
+package org.launchcode.models.forms;
+
+import org.launchcode.models.Objects.User;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class ReviewForm {
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    @Size(min=3, max=30)
+    private String coffeeName;
+
+    private String drinkType;
+
+    private String origin;
+
+    private Double rating;
+
+    private String brewDevice;
+
+    private String roaster;
+
+    private String cafe;
+
+    private String flavors;
+
+    private String roast;
+
+    public ReviewForm(){}
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getCoffeeName() {
+        return coffeeName;
+    }
+
+    public void setCoffeeName(String coffeeName) {
+        this.coffeeName = coffeeName;
+    }
+
+    public String getDrinkType() {
+        return drinkType;
+    }
+
+    public void setDrinkType(String drinkType) {
+        this.drinkType = drinkType;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    public String getBrewDevice() {
+        return brewDevice;
+    }
+
+    public void setBrewDevice(String brewDevice) {
+        this.brewDevice = brewDevice;
+    }
+
+    public String getRoaster() {
+        return roaster;
+    }
+
+    public void setRoaster(String roaster) {
+        this.roaster = roaster;
+    }
+
+    public String getCafe() {
+        return cafe;
+    }
+
+    public void setCafe(String cafe) {
+        this.cafe = cafe;
+    }
+
+    public String getFlavors() {
+        return flavors;
+    }
+
+    public void setFlavors(String flavors) {
+        this.flavors = flavors;
+    }
+
+    public String getRoast() {
+        return roast;
+    }
+
+    public void setRoast(String roast) {
+        this.roast = roast;
+    }
+}
